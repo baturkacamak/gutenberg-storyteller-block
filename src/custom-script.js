82322812937cae08d1bb8cb70f2555e68aab8bde
baturkacamak <batisonline@gmail.com>
2023-05-06 14:44:54 +0300

    Update custom-script.js to fetch categories and improve image loading
    - Refactored the `fetchPostsAndCreateStory` function to accept a `storyContainer` argument, removing the previous code that created and appended the story container.
    - Added a new function `initCategorySwiper` to fetch categories and create a Swiper instance for each category, also creating a full-screen modal container and a category Swiper container within it.
    - Updated the code in the `fetchPostsAndCreateStory` function to load images using the `data-src` attribute, and added a `Promise.all` to wait for all images to load before initializing the Swiper.
    - Modified the click event listener attached to the `.storyteller-grid-item` elements to call `initCategorySwiper` instead of the previous `fetchPostsAndCreateStory` function.
    - Added an event listener for the 'keydown' event to close the modal when the 'Escape' key is pressed.
    
    These changes improve the performance of the Gutenberg Storyteller Block by optimizing image loading and providing a better user experience with the full-screen modal and category Swiper.

--- src/custom-script.js
+++ src/custom-script.js
@@ -1,20 +1,18 @@
 // Function to fetch posts from a category and create the story slider
-async function fetchPostsAndCreateStory(categoryId) {
+async function fetchPostsAndCreateStory(categoryId, storyContainer) {
 	const response = await fetch('/wp-json/wp/v2/posts?categories=' + categoryId + '&_embed');
 	const posts = await response.json();
 
-	// Create the Instagram story container and slider
-	const storyContainer = document.createElement('div');
-	storyContainer.classList.add('storyteller-story-container');
+	// Update the story container with the slider
 	storyContainer.innerHTML = `
-    <div class="swiper-container fixed top-0 left-0 max-h-screen max-w-screen">
+    <div class="swiper h-full">
       <div class="swiper-wrapper">
         ${posts
 		.map(
 			(post) => `
-          <div class="swiper-slide">
-            <div class="storyteller-story-item">
-             	<img class="w-screen" src="${post._embedded['wp:featuredmedia'][0].source_url}" alt="${post.title.rendered}">
+          <div class="swiper-slide h-full">
+            <div class="storyteller-story-item h-full flex items-center justify-center">
+              <img data-src="${post._embedded['wp:featuredmedia'][0].source_url}" alt="${post.title.rendered}">
             </div>
           </div>`
 		)
@@ -25,25 +23,89 @@ async function fetchPostsAndCreateStory(categoryId) {
     </div>
   `;
 
-	// Append the story container to the body
-	document.body.appendChild(storyContainer);
+	// Wait for all images to load
+	const imageElements = storyContainer.querySelectorAll('img[data-src]');
+	const imageLoadPromises = Array.from(imageElements).map((img) => {
+		return new Promise((resolve) => {
+			img.onload = () => resolve();
+			img.src = img.getAttribute('data-src');
+		});
+	});
+
+	await Promise.all(imageLoadPromises);
 
 	// Initialize the Swiper.js slider
-	const swiper = new Swiper('.swiper-container', {
+	const swiper = new Swiper(storyContainer.querySelector('.swiper'), {
 		navigation: {
 			nextEl: '.swiper-button-next',
 			prevEl: '.swiper-button-prev',
 		},
 	});
+}
 
-	console.log(swiper)
+async function initCategorySwiper() {
+	const response = await fetch('/wp-json/wp/v2/categories');
+	const categories = await response.json();
+
+	// Create the full-screen modal container
+	const modalContainer = document.createElement('div');
+	modalContainer.classList.add('storyteller-modal-container', 'fixed', 'inset-0', 'z-50', 'flex', 'items-center', 'justify-center', 'bg-black', 'bg-opacity-75');
+
+	const categorySwiperContainer = document.createElement('div');
+	categorySwiperContainer.classList.add('swiper-container');
+	categorySwiperContainer.innerHTML = `
+    <div class="swiper-wrapper">
+      ${categories.map(
+		(category) => `
+        <div class="swiper-slide" data-category-id="${category.id}">
+          <div class="storyteller-story-container h-full"></div>
+        </div>`
+	).join('')}
+    </div>
+    <div class="swiper-pagination"></div>
+  `;
+
+	// Append the category swiper container to the modal container
+	modalContainer.appendChild(categorySwiperContainer);
+
+	// Append the modal container to the body
+	document.body.appendChild(modalContainer);
+
+	const categorySwiper = new Swiper('.swiper-container', {
+		slidesPerView: 3,
+		spaceBetween: 30,
+		centeredSlides: true,
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+		on: {
+			slideChange: async function () {
+				const categoryId = this.slides[this.activeIndex].getAttribute('data-category-id');
+				const storyContainer = this.slides[this.activeIndex].querySelector('.storyteller-story-container');
+				await fetchPostsAndCreateStory(categoryId, storyContainer);
+			},
+		},
+	});
+
+	// Initialize the first category's inner Swiper
+	const firstCategoryId = categories[0].id;
+	const firstStoryContainer = categorySwiperContainer.querySelector('.storyteller-story-container');
+	await fetchPostsAndCreateStory(firstCategoryId, firstStoryContainer);
+
+	// Close the modal when the Esc key is pressed
+	document.addEventListener('keydown', function (event) {
+		if (event.key === 'Escape') {
+			modalContainer.remove();
+		}
+	});
 }
 
 // Attach click event listeners to the grid items
 document.querySelectorAll('.storyteller-grid-item').forEach((item) => {
-	item.addEventListener('click', (e) => {
+	item.addEventListener('click', async (e) => {
 		e.preventDefault();
 		const categoryId = e.currentTarget.getAttribute('data-category-id');
-		fetchPostsAndCreateStory(categoryId).then(r => console.log(r));
+		await initCategorySwiper();
 	});
 });
